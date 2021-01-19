654a87b9980cd4fd66685b66133bc6529a32e30a
Minnic27 <jdCabig@mcm.edu.ph>
2021-01-20 00:16:32 +0800

    initial / commit

---
+++ Summing_Forces/Assets/Scripts/BlinkTextUI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkTextUI : MonoBehaviour
+{
+    Text textUI;
+    public GameObject spaceship;
+    public bool isTrue;
+    private bool blink;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textUI = GetComponent<Text>();
+        spaceship = GameObject.Find("StarSparrow1");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blink = spaceship.GetComponent<PhysicsMovement>().netForceCheck;
+        if (blink) {
+            StartCoroutine(BlinkSuccess());
+            blink = false;
+        } else {
+            StartCoroutine(BlinkFail());
+        }
+    }
+
+
+    IEnumerator BlinkSuccess() {
+
+        while (true) {
+
+            textUI.text = " ";
+            yield return new WaitForSeconds(1f);
+            textUI = GetComponent<Text>();
+            textUI.color = Color.green;
+            textUI.text = "Success!";
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    IEnumerator BlinkFail() {
+
+        while (true) {
+
+            textUI.text = " ";
+            yield return new WaitForSeconds(1f);
+            textUI = GetComponent<Text>();
+            textUI.color = Color.red;
+            textUI.text = "Unbalanced Force Detected";
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
---
+++ Summing_Forces/Assets/Scripts/PhysicsMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour
+{
+    public Vector3 velocityVector;
+    public Vector3 netForceVector;
+    public bool netForceCheck;
+    public List<Vector3> forceVectorList = new List<Vector3>();
+
+    
+    void FixedUpdate()
+    {
+        AddForces();
+        if(netForceVector == Vector3.zero) {
+            transform.position += velocityVector * Time.deltaTime;
+            netForceCheck = true;
+        } else {
+            Debug.LogError("Unbalanced Force Detected!");
+            netForceCheck = false;
+        }
+    }
+
+    void AddForces() {
+        netForceVector = Vector3.zero;
+
+        foreach (Vector3 forceVector in forceVectorList) {
+            netForceVector += forceVector;
+        }
+
+    }
+}
